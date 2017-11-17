e3d93bc47ec4bd8825fc17be1b39709b7d689fa2
Mario Giugno <mariogiugno@gmx.de>
2017-11-17 23:19:50 +0100

    Added test handlers for ms-species-stats

---
+++ packages/ms-species-stats/models/species.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+    this._id = null;
+    this.name = null;
+}
--- packages/ms-species-stats/routes/index.js
+++ packages/ms-species-stats/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
+var Species = require('../models/species');
+var Datastore = require('nedb');
+
 var router = express.Router();
+var db = new Datastore({
+  filename: 'store/playerSpecies.db',
+  autoload: true
+});
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('index');
+router.get('/', function(req, res) {
+  db.find({ }, function(error, docs) {
+    if(error) {
+      res.status(500).send(`Could not retrieve data. ${error}`)
+    } else {
+      res.send(docs);
+    }
+  })
+});
+
+router.post('/', function(req, res) {
+  console.log(132);
+  console.log(req.body);
+
+  var newSpecies = { _id: req.body.id, name: req.body.name };
+  db.insert(newSpecies, function(error) {
+    if(error) {
+      res.status(500).send(`Could not create new data from ${req.body}. ${error}`)
+    } else {
+      res.send('post ' + req.params + req.body);
+    }
+  });
+});
+
+router.get('/:playerId', function(req, res) {
+  db.find({ _id: req.params.playerId }, function(error, docs) {
+    if(error) {
+      res.status(500).send(`Could not retrieve data from ${req.params.playerId}. ${error}`)
+    } else {
+      res.send('get ' + req.params.playerId + docs);
+    }
+  })
 });
 
 module.exports = router;
